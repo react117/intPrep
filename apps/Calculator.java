9e3971dbb72fd4beeba84b89516c18f58f635785
react117 <30906514+react117@users.noreply.github.com>
2025-08-20 10:35:04 +0530

    Calculator PR Resolved!

--- apps/Calculator.java
+++ apps/Calculator.java
@@ -1,6 +1,5 @@
 /* Problem Statement
  * ------------------
- * 
  * Write a small calculator where the simpler equation can be supplied as a string
  * It will support +, -, *, / and understand precedence of each (precedence is a stretch goal)
  * the code should be modular and extendable
@@ -25,6 +24,9 @@
  *                 and operand variable to their default values (+ and 0)
  *      1c: If we hit end of string, we perform a summation on all the values stored in the stack, and 
  *          return the result. That's our output.
+ * ----------------------------------------------------------------------------------------------------
+ * 
+ * Next is bracket (will use recursion)
  */
 
 package apps;
@@ -32,59 +34,118 @@ package apps;
 import java.util.Stack;
 
 public class Calculator {
-    // First we're gonna parse the input string. We'll also initialise a stack, an operator and an operand variable 
-    // with default values. For the stack0 management (what and when to push or pop), we'll call pushToStack() 
-    // and pass the current operator, operand and the stack (Step 0, 1 and 1c logic here)
-    private static int calculate(String inputString) {
+    /**
+     * Parses the given expression string. We'll also initialise a stack, an operator and an operand variable
+     * with default values. For the stack0 management (what and when to push or pop), we'll call pushToStack() 
+     * and pass the current operator, operand and the stack (Step 0, 1 and 1c logic here)
+     * @param expression The given expression that needs to be evaluated.
+     * @return the value after evaluation if the input expression, 0 otherwise.
+     */
+    private static void evaluate(String expression) {
         // Initialise a stack, an operator and an operand variable with default values.
         Stack<Integer> calculateStack = new Stack();
-        int currentOperand = 0;
+        int currentOperand = 0, finalResult = 0,  operatorCount = 0, operandCount = 0;
         char currentOperator = '+';
 
-        // We need an index to parse the string, cause it's string and not an array
-        int strIndex = 0;
+        // trim blanks, saves unnecessary iterations
+        expression = expression.trim();
 
-        // Parse inputString
-        while(strIndex < inputString.length()) {
-            // System.out.println("Current char: " + inputString.charAt(strIndex) + "\n");
+        // Error if expression is empty
+        if (expression.isEmpty()) {
+            System.out.println("The input expression is empty.");
+            return;
+        } else { // Parse otherwise
+            for(int strIndex = 0; strIndex < expression.length(); strIndex++) {
+                // If the expression starts with an operator, we'll throw error
+                if(strIndex == 0 && (expression.charAt(strIndex) == '*' || expression.charAt(strIndex) == '/')) {
+                    System.out.println("The input expression cannot begin with multiplication or division.");
+                    return;
+                }
 
-            // Check whether it's a number
-            if(Character.isDigit(inputString.charAt(strIndex))) {
-                // If the number is more than 1 digit we need to make that number cuase our input is coming as a string
-                currentOperand = (currentOperand * 10) + (inputString.charAt(strIndex) - '0');
+                // if(strIndex == expression.length() -1 && isOperator(expression.charAt(strIndex))) {
+                //     break;
+                // }
+
+                if(expression.charAt(strIndex) != ' ') {
+                    // System.out.println("Current char is: " + expression.charAt(strIndex) + " at Index: " + strIndex + " and it's numeric value: " + Character.getNumericValue(expression.charAt(strIndex)) + "\n");
+
+                    // Check whether it's a number " 2 + 24 / 6 - 9 * "
+                    if(Character.isDigit(expression.charAt(strIndex))) {
+                        // If the number is more than 1 digit we need to make that number cuase our input is coming as a string
+                        currentOperand = (currentOperand * 10) + Character.getNumericValue(expression.charAt(strIndex));
+
+                        // Increase operand count
+                        operandCount++;
+                    } else if(isOperator(expression.charAt(strIndex))) { // If it's an operator, it's time to push into the stack
+                        pushToStack(currentOperator, currentOperand, calculateStack);
+
+                        // Increase operator count
+                        operatorCount++;
+
+                        // we're setting the current operator after the push because this operator is for the next operand
+                        currentOperator = expression.charAt(strIndex);
+
+                        // restore the currentOperand variable to their default values
+                        currentOperand = 0;
+                    }
+
+                    // If we're at the end of the string, it's time to push into the stack
+                    if(strIndex == expression.length() - 1) {
+                        if(isOperator(expression.charAt(strIndex))) {
+                            // Increase operator count
+                            operatorCount++;
+
+                            break;
+                        }
+
+                        pushToStack(currentOperator, currentOperand, calculateStack);
+                        
+                        // Increase operand count
+                        operandCount++;
+                    }
+                }
             }
 
-            // Check whether it's an operator or blank, if it's an operator, it's time to push into the stack
-            if(!Character.isDigit(inputString.charAt(strIndex)) && inputString.charAt(strIndex) != ' ') {
-                pushToStack(currentOperator, currentOperand, calculateStack);
+            // System.out.println(Arrays.toString(calculateStack.toArray()));
+            // System.out.println("Total operators = " + operatorCount + " and Total Operands = " + operandCount);
 
-                // we're setting the current operator after the push because this operator is for the next operand
-                currentOperator = inputString.charAt(strIndex);
-
-                // restore the currentOperand variable to their default values
-                currentOperand = 0;
+            // Calculate and return final result
+            if (operatorCount == 0) {
+                System.out.println("Input expression cannot contain of only operands!");
+                return;
+            } else if (operandCount == 0) {
+                System.out.println("Input expression cannot contain of only operators!");
+                return;
+            } else if (!calculateStack.empty()) {
+                finalResult = finalResult(calculateStack);
             }
 
-            // If we're at the end of the string, it's time to push into the stack
-            if(strIndex == inputString.length() - 1) {
-                pushToStack(currentOperator, currentOperand, calculateStack);
-            }
-
-            strIndex++;
-        }
-
-        // System.out.println(Arrays.toString(calculateStack.toArray()));
-
-        // Calculate and return final result
-        if (!calculateStack.empty()) {
-            return finalResult(calculateStack);
-        } else {
-            return 0;
+            System.out.println("The result is (drumroll sound): " + finalResult);
         }
     }
+    
+    /**
+     * Checks if the given character is an operator.
+     * @param element The value to check.
+     * @return true if the element is an operator [+, -, *, /], false otherwise.
+     */
+    private static boolean isOperator(char element) {
+        if(element != ' ' && (element == '+' || element == '-' || element == '*' || element == '/')) {
+            return true;
+        }
 
-    // Here we're gonna decide what to insert into the stack (Step 1a - 1b full logic here)
+        return false;
+    }
+
+    /**
+     * Here we're gonna decide what to insert into the stack (Step 1a - 1b full logic here)
+     * @param currentOperator An operator that needs to be calculated.
+     * @param currentOperand  An operand that needs to be calculated.
+     * @param calculateStack  A stack which stores operators and operands that needs to be summed.
+     */
     private static void pushToStack(char currentOperator, int currentOperand, Stack<Integer> calculateStack) {
+        // System.out.println("currentOperator: " + currentOperator + " currentOperand: " + currentOperand + "\n");
+
         // Check for current operator, it it's + or -, we simply multiply currentOperand by +1 or -1 accordingly
         if(currentOperator == '+') {
             currentOperand = currentOperand * 1;
@@ -106,13 +167,14 @@ public class Calculator {
 
         calculateStack.push(currentOperand);
 
-        // System.out.println("currentOperator: " + currentOperator + " currentOperand: " + currentOperand + "\n");
         // System.out.println(Arrays.toString(calculateStack.toArray()));
-
-
     }
 
-    // Calculate sumation of the stack values and return final result (step 1c logic here)
+    /**
+     * Calculate sumation of the stack values and return final result (step 1c logic here)
+     * @param calculateStack Stack containing all values that needs to be summed up.
+     * @return Summation of the values stored in the stack.
+     */
     private static int finalResult(Stack<Integer> calculateStack) {
         // System.out.println("Final Stack: " + Arrays.toString(calculateStack.toArray()));
         
@@ -129,12 +191,10 @@ public class Calculator {
     // Here we're gonna take the input string from user and send it to calculate method for the result.
     public static void main(String[] args) {
         // Input string (Static for time being)
-        String inputStr = "2 + 24 / 6 - 9 * 2";
+        String expression = " 2 + 24 / 6 - 9 * 2 ";
 
-        // Call calculate() method cause main() don't wanna do stuff on it's own in the name of modulation :P
-        int result = calculate(inputStr);
-
-        System.out.println("The result is (drumroll sound): " + result);
+        // Call evaluate() method cause main() don't wanna do stuff on it's own in the name of modulation :P
+        evaluate(expression);
     }
 }
 
